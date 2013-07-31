e21ad25f0e8344f2d2f5124f177368329bf5f4e6
Nigel Lundsten <nigel.lundsten@gmail.com>
2013-07-31 09:35:17 -0700

    fix params used in invoke method, add more methods to make rendering of views more flexible

--- src/SpeckRandomProducts/View/Helper/Product.php
+++ src/SpeckRandomProducts/View/Helper/Product.php
@@ -4,24 +4,49 @@ namespace SpeckRandomProducts\View\Helper;
 
 use Zend\View\Helper\HelperInterface;
 use Zend\View\Helper\AbstractHelper;
+use Zend\View\Model\ViewModel;
 
 class Product extends AbstractHelper
 {
     protected $products;
     protected $template;
     protected $callable;
-    protected $params=array();
+    protected $params = array();
     protected $productService;
+    protected $clips = array();
+    protected $views = array();
 
     public function __invoke($params=null)
     {
         $params = ($params ?: $this->params);
-        $products = call_user_func_array($this->callable, $this->params);
+        $products = call_user_func_array($this->callable, $params);
 
         $html = '';
         foreach ($products as $product) {
             $product = $this->getProductService()->getFullProduct($product->getProductId());
-            $html .= $this->getView()->render($this->template, array('product' => $product));
+            $view = new ViewModel(array('product' => $product));
+            $this->views[] = $view->setTemplate($this->getTemplate());
+        }
+        return $this;
+    }
+
+    public function renderViewsToClips($returnClips = false)
+    {
+        foreach ($this->getViews() as $view) {
+            $this->clips[] = $this->getView()->render($view);
+        }
+        if ($returnClips) {
+            return $this->getClips();
+        }
+        return $this;
+    }
+
+    public function __toString()
+    {
+        $html = '';
+        $clips = $this->renderViewsToClips(true);
+        foreach ($clips as $clip) {
+            $html .= $this->getView()->render($view);
         }
         return $html;
     }
@@ -95,6 +120,42 @@ class Product extends AbstractHelper
     public function setProductService($productService)
     {
         $this->productService = $productService;
+        return $this;
+    }
+
+    /**
+     * @return clips
+     */
+    public function getClips()
+    {
+        return $this->clips;
+    }
+
+    /**
+     * @param $clips
+     * @return self
+     */
+    public function setClips($clips)
+    {
+        $this->clips = $clips;
+        return $this;
+    }
+
+    /**
+     * @return views
+     */
+    public function getViews()
+    {
+        return $this->views;
+    }
+
+    /**
+     * @param $views
+     * @return self
+     */
+    public function setViews($views)
+    {
+        $this->views = $views;
         return $this;
     }
 }
